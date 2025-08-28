1b90732c8a86de65a5a8e59c7b07df0cff0965ee
bexsadirov <beksultan.sadirov@gmail.com>
2025-08-28 19:26:04 +0500

    initial commit

---
+++ src/main/java/kz/bailanysta/main/BailanystaBackendApplication.java
@@ -0,0 +1,13 @@
+package kz.bailanysta.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BailanystaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BailanystaBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kz/bailanysta/main/BailanystaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.bailanysta.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BailanystaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
